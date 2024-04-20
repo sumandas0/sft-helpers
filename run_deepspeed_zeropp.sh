ec7691b31d41ceb6709a8556876fd4b28198bdce
Suman Das <59254445+sumandas0@users.noreply.github.com>
2024-04-20 14:41:15 +0530

    first commit

---
+++ run_deepspeed_zeropp.sh
@@ -0,0 +1,31 @@
+accelerate launch --config_file "configs/deepspeed_zeropp_config.yaml"  train.py \
+--seed 100 \
+--model_name_or_path "mistralai/Mistral-7B-v0.1" \
+--dataset_name "HuggingFaceH4/ultrachat_200k" \
+--chat_template_format "chatml" \
+--add_special_tokens False \
+--append_concat_token False \
+--splits "train_sft,test_sft" \
+--max_seq_len 2048 \
+--num_train_epochs 1 \
+--logging_steps 5 \
+--log_level "info" \
+--logging_strategy "steps" \
+--evaluation_strategy "epoch" \
+--save_strategy "epoch" \
+--push_to_hub \
+--hub_private_repo True \
+--hub_strategy "every_save" \
+--bf16 True \
+--packing True \
+--learning_rate 5e-6 \
+--lr_scheduler_type "cosine" \
+--weight_decay 0.0 \
+--warmup_ratio 0.1 \
+--max_grad_norm 1.0 \
+--output_dir "mistral-sft-ds-zpp" \
+--per_device_train_batch_size 2 \
+--gradient_accumulation_steps 4 \
+--gradient_checkpointing False \
+--dataset_text_field "content" \
+--use_flash_attn True
---
+++ run_fsdp.sh
@@ -0,0 +1,33 @@
+accelerate launch --config_file "configs/fsdp_config.yaml"  train.py \
+--seed 100 \
+--model_name_or_path "mistralai/Mistral-7B-v0.1" \
+--dataset_name "HuggingFaceH4/ultrachat_200k" \
+--chat_template_format "chatml" \
+--add_special_tokens False \
+--append_concat_token False \
+--splits "train_sft,test_sft" \
+--max_seq_len 2048 \
+--num_train_epochs 1 \
+--logging_steps 5 \
+--log_level "info" \
+--logging_strategy "steps" \
+--evaluation_strategy "epoch" \
+--save_strategy "epoch" \
+--push_to_hub \
+--hub_private_repo True \
+--hub_strategy "every_save" \
+--bf16 True \
+--packing True \
+--learning_rate 2e-5 \
+--lr_scheduler_type "cosine" \
+--weight_decay 0.0 \
+--warmup_ratio 0.1 \
+--max_grad_norm 1.0 \
+--output_dir "mistral-sft-fsdp" \
+--per_device_train_batch_size 16 \
+--per_device_eval_batch_size 16 \
+--gradient_accumulation_steps 4 \
+--gradient_checkpointing True \
+--use_reentrant False \
+--dataset_text_field "content" \
+--use_flash_attn True
